c243d7c78ed9f919358d63840206117ee475649f
george <george510257@gmail.com>
2022-06-13 09:57:06 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import {AlertComponent, ToastComponent} from "amis";
-import AmisComponent from "./component/AmisComponent";
+import logo from './logo.svg';
+import './App.css';
 
-import "amis/lib/themes/default.css"
-
-class App extends React.Component<any, any> {
-
-    render() {
-        return (
-            <div className="App">
-                <ToastComponent key="toast" position={'top-right'}/>
-                <AlertComponent key="alert"/>
-                <AmisComponent/>
-            </div>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
